54bb0c1b7c2f0c89b6abc3d2d59c98aa611075b5
Ana Paula <aninha10.desouza@gmail.com>
2023-03-25 22:26:06 -0300

    css feito

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { Image, Text, View, StyleSheet, ScrollView } from 'react-native';
-// import Constants from 'expo-constants'
+import { Button, Icon } from 'expo-constants';
+
 function Card(props) {
   return (
     <View style={styles.card}> 
@@ -7,10 +8,9 @@ function Card(props) {
         source={{uri: props.animal.capa}}
         style={styles.animal}
       />
-      <Text style={{ color: 'darked' }}> {props.animal.nome}</Text>
-      <Text style={{ fontSize: 12 }}> {props.animal.raca}</Text>
-    </View>
-    
+      <Text style={styles.textocard}> {props.animal.nome}</Text>
+      <Text style={styles.textocard}> {props.animal.raca}</Text>
+    </View>   
   );
 }
 
@@ -18,27 +18,27 @@ export default function App() {
   const animais = [
     {
       capa: 'https://adotar.com.br/painel/upload/2022-07/animais_imagem871832.jpg', 
-      nome: 'Animal 1', 
+      nome: 'Totó', 
       raca: 'Viralata',
-    },,
+    },
     {
       capa: 'https://f.i.uol.com.br/fotografia/2013/05/28/281341-600x600-1.jpeg', 
-      nome: 'Animal 2', 
+      nome: 'Bob', 
       raca: 'Viralata',
     },
     {
       capa: 'https://www.petz.com.br/blog/wp-content/uploads/2021/03/gato-desaparecido-3-1.jpg', 
-      nome: 'Animal 3', 
+      nome: 'Biscoito', 
       raca: 'Viralata',
     },
     {
       capa: 'https://img.freepik.com/fotos-premium/pobre-gato-em-um-abrigo-privado-da-cidade-de-odessa-gatos-perdidos-e-abandonados-devido-a-guerra-na-ucrania_713948-439.jpg?w=2000', 
-      nome: 'Animal 4', 
+      nome: 'Mel', 
       raca: 'Viralata',
     },
     {
       capa: 'https://i0.statig.com.br/bancodeimagens/c8/j5/6q/c8j56qaz2yrps8wgrm7bzyyfp.jpg', 
-      nome: 'Animal 5', 
+      nome: 'Lili', 
       raca: 'Viralata',
     },
     
@@ -46,9 +46,11 @@ export default function App() {
   return (
     <ScrollView style={styles.ScrollView}>
       <View style={styles.container}>
-        <Text style={{ fontSize: 25, fontWeigth: 'bold' }}> Olá Mundo!!</Text>
-      <View style={styles.conteudo}>
+        <Text style={styles.titulo}> Amigos de Joinville</Text>
+        <Text style={styles.titulo2}> Animais Disponíveis:</Text>
+            <View style={styles.conteudo}>
        { animais.map(animal => <Card animal={animal} />)}
+       
       </View>
    </View>
    </ScrollView>
@@ -59,29 +61,54 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
     paddingTop: 20,
+    paddingBottom: 50,
   },
   conteudo: {
     flex: 1,
-    backgroundColor: '#ccc',
     flexDirection: 'row',
     flexWrap: 'wrap',
     width: '100%',
+
+  },
+  titulo: {
+    backgroundColor: '#FA497A',
+    fontWeight: 'bold',
+    fontSize: 30,
+    textAlign: 'center',
+    paddingTop: 10,
+    color: 'white',
+  },
+  titulo2: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'black',
+    textAlign: 'center',
+    paddingTop: 10,
   },
   card: {
     backgroundColor: 'pink',
-    width: '40%',
-    height: 250,
-    borderRadius: 10,
-    margin: 15
+    width: '42%',
+    height: 260,
+    borderRadius: 30,
+    margin: 15,
+    textAlign: 'center',
   },
   ScrollView: {
-    marginHorizontal: 20,
+    marginHorizontal: 0,
   },
   animal: {
     resizeMode: 'stretch',
-    width: '100%',
+    width: '87%',
+    margin: 10,
     height: 200,
-  } 
+    borderRadius:30,
+  },
+  textocard: {
+    textAlign: 'center',
+    fontSize: 12,
+  },
+  botao: {
+    backgroundColor: 'white',
+  }
 });
